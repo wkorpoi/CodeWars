4d8611c8dc640583c9b66edf9f782dd13e56f590
wadiya korpoi <wadiyakorpoi@gmail.com>
2023-04-11 10:58:26 -0400

    daily code

---
+++ FindTheFirstNonConsecNum.js
@@ -0,0 +1,21 @@
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+// 1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
+
+// 2
+// Swift, Ruby and Crystal: nil
+// Haskell: Nothing
+// Python, Rust, Scala: None
+// Julia: nothing
+// Nim: none(int) (See options)
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  }
